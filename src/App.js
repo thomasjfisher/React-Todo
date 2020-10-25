65a8da1b5d026ca10f7f6e45d9a013e9cc61245e
Thomas Fisher <thomasjfisher1@gmail.com>
2020-10-25 16:14:00 +1100

    finished todo app with react. will add styling later

--- src/App.js
+++ src/App.js
@@ -2,11 +2,29 @@ import React, { useState, useRef, useEffect } from 'react';
 import TodoList from './TodoList'
 import {v4 as uuidv4} from 'uuid'
 
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
+
 function App() {
   const [todos, setTodos] = useState([])
   const todoNameRef = useRef()
 
-  
+  // the useEffect is storing to local storage in a few lines of code
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+      if(storedTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+
+  }, [todos])
+
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id ===id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
 
   function handleAddTodo(e) {
     const name = todoNameRef.current.value
@@ -16,13 +34,19 @@ function App() {
     })
     todoNameRef.current.value = null
   }
+  function handleClearTodos() {
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
+
   return (
     <>
-    <TodoList todos={todos} />
+    <TodoList todos={todos} toggleTodo={toggleTodo} />
     <input ref={todoNameRef}type="text" />
     <button onClick={handleAddTodo}>Add Todo</button>
-    <button>Clear Todos</button>
-    <div>0 left todo</div>
+    <button onClick={handleClearTodos}>Clear Todos</button>
+    <div>{todos.filter(todo => !todo.complete).length} left todo</div>
+
     </>
   )
 }
